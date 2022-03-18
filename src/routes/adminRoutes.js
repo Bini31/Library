b2ed1eb57fad3ef1dd714cae6984976e9cb72f07
Bini31 <71607307+Bini31@users.noreply.github.com>
2022-03-18 17:25:57 +0400

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,30 +1,22 @@
 const express=require('express');
 const adminRouter=express.Router();
 const Bookdata=require('../model/Bookdata');
-function router(nav){
+function router(nav,upload){
 adminRouter.get('/',function(req,res){
     res.render('addBook',{
         nav,
+        upload,
         title:'Library'
     })
 })
-adminRouter.post('/add',function(req,res){
-   // res.send("Hey iam added");
-   /*var item={
-    title:req.query.title,
-    author:req.query.author,
-    genre:req.query.genre,
-    image:req.query.image
-   }
-   var book=Bookdata(item);
-   book.save();//saving to database
-   res.redirect('/books');*/
-   //res.send("Hey iam added|||");
+adminRouter.post('/add',upload.single('image'),function(req,res){
+  
+  
    var item={
     title:req.body.title,
     author:req.body.author,
     genre:req.body.genre,
-    image:req.body.image
+    image:req.file.originalname
    }
    var book=Bookdata(item);
    book.save();//saving to database
