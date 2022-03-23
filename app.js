80a26c8b6676df2ca3efca2c02fad639f7c82a7c
Bini31 <71607307+Bini31@users.noreply.github.com>
2022-03-23 12:27:10 +0400

    Update app.js

--- app.js
+++ app.js
@@ -88,5 +88,5 @@ res.render("home",
         })*/
         const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
-    console.log('Our Library app is running on port${PORT}');
+    console.log('Our Library app is running on '+PORT);
 });
